3776f49e7eca0ffebe7816f146fc7c3dbafa7148
scrypnyc <alexanderlookua@gmail.com>
2019-02-21 16:54:30 +0200

    Initial Commit

---
+++ soundPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  soundPad
+//
+//  Created by Alexander Skrypnyk on 2/21/19.
+//  Copyright © 2019 skrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
