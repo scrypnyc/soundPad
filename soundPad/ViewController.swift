bfb4c32d5a5fa16c287bf903d7d8f1b4ca2eca10
scrypnyc <alexanderlookua@gmail.com>
2019-02-21 19:49:22 +0200

    adding sound and final code

--- soundPad/ViewController.swift
+++ soundPad/ViewController.swift
@@ -7,14 +7,101 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func aTapped(_ sender: UIButton) {
+        
+        
+        // Getting the url
+        let url = Bundle.main.url(forResource: "1", withExtension: "wav")
+        
+        
+        // Make sure that we've got the url, otherwise abort
+        guard url != nil else {
+            return
+        }
+        
+        // Create the audio player and play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
+    }
+    
+    @IBAction func bTapped(_ sender: UIButton) {
+        
+        // Getting the url
+        let url = Bundle.main.url(forResource: "2", withExtension: "wav")
+        
+        
+        // Make sure that we've got the url, otherwise abort
+        guard url != nil else {
+            return
+        }
+        
+        // Create the audio player and play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
+    }
+    @IBAction func cTapped(_ sender: UIButton) {
+        
+        // Getting the url
+        let url = Bundle.main.url(forResource: "3", withExtension: "mp3")
+        
+        
+        // Make sure that we've got the url, otherwise abort
+        guard url != nil else {
+            return
+        }
+        
+        // Create the audio player and play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
+    }
+    @IBAction func dTapped(_ sender: UIButton) {
+        
+        // Getting the url
+        let url = Bundle.main.url(forResource: "4", withExtension: "wav")
+        
+        
+        // Make sure that we've got the url, otherwise abort
+        guard url != nil else {
+            return
+        }
+        
+        // Create the audio player and play the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
+    }
+    
+    
+    
+    
 }
 
